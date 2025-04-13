8afe0fe7a6158f4cb92dca2ed4ce60a3aee1f9c7
Marcela Silvério <marcelapsilverio@gmail.com>
2025-04-13 19:06:53 -0300

    feat: migrations done

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -30,6 +30,7 @@ class User
      */
     protected $hidden = [
         'password',
+        'remember_token',
     ];
 
     /**
@@ -40,6 +41,7 @@ class User
     protected function casts(): array
     {
         return [
+            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
