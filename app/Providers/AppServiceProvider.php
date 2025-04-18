c8a68e07b03be933bb9724c9513874a9726c9c48
Marcela Silvério <marcelapsilverio@gmail.com>
2025-04-18 18:08:44 -0300

    feat: organizing dependency injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,14 +4,41 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use App\Events\PaymentCreatedEvent;
+
+use ExternalAuthorizationApiService;
+use ExternalNotificationApiService;
+use App\Interfaces\PaymentAuthorizationServiceInterface;
+use App\Interfaces\NotificationServiceInterface;
+use App\Services\Payment\PaymentService;
+use App\Validators\Payment\PaymentServiceValidator;
+use App\Repositories\Payment\PaymentRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(PaymentServiceValidator::class, function ($app) {
-            return new PaymentServiceValidator();
+            return new PaymentServiceValidator($app->make(PaymentAuthorizationServiceInterface::class));
+        });
+
+        $this->app->singleton(PaymentRepository::class, function ($app) {
+            return new PaymentRepository();
+        });
+
+        $this->app->singleton(PaymentService::class, function ($app) {
+            return new PaymentService(
+                $app->make(PaymentServiceValidator::class),
+                $app->make(PaymentRepository::class),
+                $app->make(NotificationServiceInterface::class)
+            );
+        });
+
+        $this->app->bind(NotificationServiceInterface::class, function ($app) {
+            return $app->make(ExternalNotificationApiService::class);
+        });
+
+        $this->app->bind(PaymentAuthorizationServiceInterface::class, function ($app) {
+            return $app->make(ExternalAuthorizationApiService::class);
         });
     }
 
